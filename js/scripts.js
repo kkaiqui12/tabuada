dd1f63cfd69630abcc00268f78c9eea9b770b98d
Kaiqui <kkaiqui.12@gmail.com>
2023-11-17 20:55:35 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+//seleção de elementos
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+const multipicationTitle = document.querySelector("#multiplication-title span")
+const multiplicationTable = document.querySelector("#multiplication-operations");
+
+//funções 
+const createTable = (number, multiplicatorNumber) => {
+  multiplicationTable.innerHTML = "";
+
+    for (i = 1; i <= multiplicatorNumber; i++) {
+      const result = number * i;
+
+      const template = 
+        `<div class="row">
+          <div class="operation">${number} x ${i} = </div>
+          <div class="result">${result}</div>
+        </div>`;
+
+      const parser =  new DOMParser()
+
+      const htmlTamplate = parser.parseFromString(template, "text/html");
+
+      const row = htmlTamplate.querySelector(".row");
+
+      multiplicationTable.appendChild(row);
+    }
+
+    multipicationTitle.innerText = number;
+};
+  
+
+
+//eventos
+multiplicationForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const multiplicationNumber = +numberInput.value;
+  const multiplicatorNumber = +multiplicationInput.value;
+
+    if(!multiplicationNumber || !multiplicatorNumber) return;
+
+  createTable(multiplicationNumber, multiplicatorNumber);
+});
